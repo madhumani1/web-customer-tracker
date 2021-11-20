e058385ffb681e1a2c7515b65b2b44fc7b03d285
Madhukar Mani <madhumani1@gmail.com>
2021-11-19 20:11:31 -0500

    MVC Hibernate (READ) List Customer and use CSS

--- src/main/java/com/madhu/demo/controller/CustomerController.java
+++ src/main/java/com/madhu/demo/controller/CustomerController.java
@@ -3,19 +3,36 @@
  */
 package com.madhu.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.madhu.demo.dao.CustomerDAO;
+import com.madhu.demo.entity.Customer;
+
 /**
  * @author 15197
  * Customer Controller
+ * With Autowired, Spring will scan for a component that implements CustomerDAO interface
  */
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	// need to inject the customer DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel)	{
+		// get customer from the DAO
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customer to my Spring MVC model
+		theModel.addAttribute("customers", theCustomers);	// customers name and theCustomers value
+		
 		return "list-customers";
 	}
 }
---
+++ src/main/java/com/madhu/demo/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.madhu.demo.dao;
+
+import java.util.List;
+
+import com.madhu.demo.entity.Customer;
+
+/**
+ * @author 15197
+ *
+ */
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+}

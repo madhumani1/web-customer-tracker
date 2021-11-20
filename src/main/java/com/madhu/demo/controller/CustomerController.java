2a96865a12181d2434acb3396eb61d24eebedeb8
Madhukar Mani <madhumani1@gmail.com>
2021-11-19 21:55:20 -0500

    Use of GetMapping/PostMapping

--- src/main/java/com/madhu/demo/controller/CustomerController.java
+++ src/main/java/com/madhu/demo/controller/CustomerController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.madhu.demo.dao.CustomerDAO;
@@ -17,6 +18,7 @@ import com.madhu.demo.entity.Customer;
  * @author 15197
  * Customer Controller
  * With Autowired, Spring will scan for a component that implements CustomerDAO interface
+ * no more RequestMapping. use GetMapping or PostMapping
  */
 @Controller
 @RequestMapping("/customer")
@@ -25,7 +27,8 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	/* @RequestMapping("/list") */
+	@GetMapping("/list")
 	public String listCustomers(Model theModel)	{
 		// get customer from the DAO
 		List<Customer> theCustomers = customerDAO.getCustomers();
